f112c3f9309f0ab1d6eb42946cc0e958e2acda2d
Ahmed El-Askary <134425344+Ahmed-El-Askary@users.noreply.github.com>
2024-03-25 01:19:35 +0200

    Add files via upload

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,29 +1,49 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-    echo "Error: Incorrect number of arguments. Number of arguments must be 2."
+# Check if the required arguments are provided
+if [ $# -lt 2 ]; then
+    echo "Error: Two arguments are required."
     exit 1
 fi
 
-writefile=$1
-writestr=$2
+# Assign the command line arguments to variables
+writefile="$1"
+writestr="$2"
 
-dirname=($(dirname ${writefile}))
-
-# Check if directory exists, if not, create it
-if [ ! -d $dirname ]; then
-    mkdir -p ${dirname}
+# Check if writefile argument is specified
+if [ -z "$writefile" ]; then
+    echo "Error: writefile argument is not specified."
+    exit 1
 fi
 
-# Create file if it does not exist
-touch ${writefile} &>/dev/null
+# Check if writestr argument is specified
+if [ -z "$writestr" ]; then
+    echo "Error: writestr argument is not specified."
+    exit 1
+fi
 
-# Write to file if it exists and user has write access
-if [ -w $writefile ]
-then
-    echo ${writestr} > ${writefile}
+# Create the directory path if it doesn't exist
+mkdir -p "$(dirname "$writefile")"
+
+# Write the content to the file
+echo "$writestr" > "$writefile"
+
+# Check if the file was created successfully
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to create the file."
+    exit 1
+fi
+
+# Print success message
+echo "File created: $writefile"
+
+
+if [[ $# -eq 0 ]] ; then
+    echo 'some message'
     exit 0
-else
-    echo "Error: file could not be written"
-    exit 1
 fi
+
+case "$1" in
+    1) echo 'you gave 1' ;;
+    *) echo 'you gave something else' ;;
+esac
